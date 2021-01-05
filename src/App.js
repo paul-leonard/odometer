6235a1e750bc1026e6b0487ab74d867bc54d0fa5
paul-leonard <paul.ryan.leonard@gmail.com>
2021-01-04 20:48:47 -0800

    Header and Footer functions created and rendering content provided by props; Odometer class rendering content provided by props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { render } from '@testing-library/react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello, Paul! </h1>
+      <Header description="This is a simple fake odometer to demonstrate use of React."/>
+      <Odometer />
+      <Footer trademark="Rolling odometer brought to you by Paul Leonard" />
     </div>
   );
+}
+
+function Header(props) {
+  return(
+    <div>
+      <h2>Wow!!! That's a lot of miles!  Let's roll them back (by wrapping)!</h2>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+
+function Footer(props) {
+  return(
+    <p>{props.trademark}</p>
+  )
+}
+
+class Odometer extends React.Component {
+
+  render() {
+    return(
+      <h1> Odometer from class </h1>
+    )};
 }
 
 export default App;
